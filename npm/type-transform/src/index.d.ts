23cd0a2339f05257246505d98479b677c7cd39db
goldenratio <karthikeyanvj@gmail.com>
2025-02-20 02:25:02 +0200

    feat: fixed node type definitions

--- npm/type-transform/src/index.d.ts
+++ npm/type-transform/src/index.d.ts
@@ -1,5 +1,14 @@
-export declare interface TrasnformOptions {
+export declare interface TransformOptions {
+  /**
+   * A banner to be added to the generated file, this can be a package path for "kotlin",
+   * a custom auto code generated message or a comment block such as a license for the code
+   */
   readonly banner?: string;
+
+  /**
+   * A footer to be added to the generated file, this can be something like a comment block for a license
+   * or just a fun easter egg
+   */
   readonly footer?: string;
 }
 
@@ -7,4 +16,7 @@ export declare interface TransformResult {
   readonly success: boolean;
 }
 
+/**
+ * Transform TypeScript types to Swift/Kotlin types
+ */
 export declare function transform(srcFilePath: string, outFilePath: string, options: TransformOptions): Promise<TransformResult>;
